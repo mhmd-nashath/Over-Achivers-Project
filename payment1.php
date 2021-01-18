bfb183b440a862c9bdac72056c2156bc71e766b6
mhmd-nashath <75944437+mhmd-nashath@users.noreply.github.com>
2021-01-18 10:02:09 +0530

    Add files via upload

---
+++ payment1.php
@@ -0,0 +1,566 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        
+     
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
+
+ <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+crossorigin="anonymous"></script>
+        
+
+<!--Javascript Link-->
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+    
+
+ <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+  />
+  
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+        <script type="text/javascript" src="js/respond.js"></script>
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+      
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+        
+        <!--CSS is Linked Below, Do not to change Name-->      
+    <link rel="stylesheet" type="text/css" href="Style.css">
+      
+    <!--Javascript is Linked Below, Do not change Name-->
+          <script src="Main.js"></script>
+       
+          <title>header & footer</title>
+      
+      <style>  
+@import url('https://fonts.googleapis.com/css?family=Roboto:700');
+/*Top Header*/
+#container{
+    padding-left: 225px;
+    padding-right: 225px;
+}
+
+#nav-line{
+    font-size: 18px;
+    line-height: 30px;
+    font-family: 'Roboto', sans-serif;
+    text-transform: uppercase;
+}
+
+#navbar{
+    border-bottom: 4px;
+    border-bottom-style: solid;
+    border-bottom-color: #010101;
+    border-top: 3px;
+    border-top-style: solid;
+    border-bottom-color: #010101;
+    border-left: 3px;
+    border-left-style: solid;
+    border-left-color: #010101;
+    border-right: 3px;
+    border-right-style: solid;
+    border-right-color: #010101;
+    background-color: #6D6B6B;
+}
+
+#nav-line:hover{
+    color: #ffffff;
+    background: #939292;
+}
+
+#head-nav{
+    font-size: 18px;
+    padding-left: 10px;
+    padding-right: 10px;
+    padding-bottom: 5px;
+}
+
+#head-nav-drop{
+    font-size: 18px;
+    padding-top: 5px;
+}
+
+.vl {
+    border-left: 3px solid black;
+    height: 20px;
+}
+
+#user{
+    overflow-y: hidden;
+}
+
+
+
+/*Top Header is finished*/
+
+/*Header NavBar*/
+.navbar-custom{
+    background-color: #DEDEDE;
+    border-bottom: 3px;
+    border-top: 2px;
+    border-left: 3px;
+    border-right: 3px;
+    border-style: solid;
+    border-top-color: #727272;
+    border-bottom-color: #727272;
+    border-right-color: #727272;
+    border-left-color: #727272;
+}
+
+#row-left{
+    margin-left: 20px;
+}
+
+#row-right{
+    float: right;
+    padding-top: 2px;
+    margin-left: 350px;
+}
+
+/*Search Bar*/
+.has-search .form-control {
+    padding-left: 2.375rem;
+}
+
+.has-search .form-control-feedback {
+    position: absolute;
+    z-index: 2;
+    display: block;
+    width: 2.375rem;
+    height: 2.375rem;
+    line-height: 2.375rem;
+    text-align: center;
+    pointer-events: none;
+    color: #aaa;
+}
+/*Search bar finishes*/
+
+#cart{
+    padding-left: 10px;
+    padding-bottom: 1px;
+}
+
+/*Header NavBar*/
+
+  
+
+/*Products Tag*/
+.legend{
+    padding-left: 0px;
+    color: #0061FF;
+}
+
+/*Footer*/
+#footer-top{
+    background-color: #DEDEDE;
+}
+
+#sub-box{
+    padding-top: 10px;
+    color: black;
+    font-size: 15px;
+}
+
+#space{
+    padding-top: 15px;
+}
+
+#social{
+    padding-left: 5px;
+}
+
+#copy-name{
+    text-decoration:none;
+    color: white;
+}
+
+/*Scroll Up*/
+#back2Top {
+    width: 40px;
+    line-height: 40px;
+    overflow: hidden;
+    z-index: 999;
+    display: none;
+    cursor: pointer;
+/*    -moz-transform: rotate(270deg);
+    -webkit-transform: rotate(270deg);
+    -o-transform: rotate(270deg);
+    -ms-transform: rotate(270deg);
+    transform: rotate(270deg);*/
+    position: fixed;
+    bottom: 50px;
+    right: 10px;
+    background-color: #ACACAC;
+    color: black;
+    text-align: center;
+    font-size: 30px;
+    text-decoration: none;
+}
+#back2Top:hover {
+    background-color:gray;
+    color: #000;
+}
+
+.navbar-brand
+{
+  font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  color:grey;
+  font-size: 24px;
+  margin: 0px;
+  width: 250px;
+
+}
+        </style>
+    </head>
+    <body>
+
+         <!-----------------------------------java script-------------------->
+
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+crossorigin="anonymous"></script>
+        
+
+<!--Javascript Link-->
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+    
+<!--Header Begins From Here...-->
+<header>
+<!--Top Header-->
+
+<!-- As a heading -->
+    <nav class="navbar navbar-expand-md sticky-top navbar-dark main-nav" id="navbar">
+        <div class="container" id="container">
+    
+            
+            <div class="navbar-collapse collapse nav-content order-2">
+                <ul class="nav navbar-nav nav-line">
+                    <li class="nav-item"><a class="nav-link" href="Index.html"><img src="New folder/logo.jpg" width="200" height="70" alt=""></a></li>    
+                    <button class="navbar-toggler ml-2" type="button" data-toggle="collapse" data-target=".nav-content" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>       
+                </ul>
+        </div>
+        <div class="ml-auto navbar-collapse collapse nav-content order-3 order-md-3">
+                
+            <nav class="navbar navbar-light bg-dark">
+                <span class="navbar-brand mb-0 h1">Online Amuthu Kade</span>
+              </nav>
+        
+        </div>
+
+          <div class="ml-auto navbar-collapse collapse nav-content order-3 order-md-3">
+            <ul class="nav navbar-nav nav-line">
+                <li class="nav-item"><a class="nav-link" href="home.html"><img src="New folder/logo2.jpg" width="200" height="70" alt=""></a></li>    
+                <button class="navbar-toggler ml-2" type="button" data-toggle="collapse" data-target=".nav-content" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>       
+            </ul>
+    </div>
+        </div>
+
+        <div class="dropdown dropleft">
+            <ul class="nav navbar-nav">
+                <li class="nav-item"><img src="New folder/avatar1.png" height="75px;" width="75px;" data-toggle="dropdown" class="dropdown-toggle"> 
+                    <ul class="dropdown-menu" id="user">
+                        <li>
+                            <a class="dropdown-item" href="form.html"><i class="fas fa-sign-in-alt"></i> Login<a class="dropdown-item" href="Sign.html"><i class="fas fa-user-plus"></i> Signup</a><a class="dropdown-item" href="home.html"><i class="fas fa-user-alt"></i> Profile</a>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>    
+    </nav>        
+    </nav>
+
+<!--Top Header is Finished-->
+    
+<!--Header Navigation Bar-->
+
+
+<nav class="navbar navbar-expand-md navbar-fixed-top navbar-light navbar-custom">
+    <div class="row">
+        <ul class="nav navbar-nav w-100" id="row-left">
+            
+            <li class="nav-item dropdown">
+                    
+                <button type="button" class="btn dropdown-toggle" data-target="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-bars" style="padding-right: 2px; padding-left: 5px; margin-left: 2px;" id="head-nav-drop"></i>  Categories
+                </button>
+                
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown1">
+                    <li><a class="dropdown-item" href="#">Computer accessories</a></li>
+                    <li><a class="dropdown-item" href="f&v.html">Fruits &amp; Vegitables</a></li>
+                    <li><a class="dropdown-item" href="#">Grocery Items</a></li>
+                    <li><a class="dropdown-item" href="#">feedback</a></li>
+                    <li><a class="dropdown-item" href="#">Manufacture details</a></li>
+                </ul>
+                
+            </li>
+            
+            <li class="nav-item"  id="head-nav">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            
+            <li class="nav-item"  id="head-nav">
+                <a class="nav-link" href="#">About us</a>
+            </li>
+            
+            <li class="nav-item"  id="head-nav">
+                <a class="nav-link" href="#">Catalog</a>
+            </li>
+            
+            <li class="nav-item"  id="head-nav">
+                <a class="nav-link" href="bestbuy.html">Best buy</a>
+            </li>
+            
+            <li class="nav-item" id="row-right">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Search...">
+                        <div class="input-group-append">
+                        <button class="btn btn-secondary" type="button">
+                        <i class="fa fa-search"></i>
+                        </button>
+                        </div>
+                </div>
+            </li>
+            
+            <li class="nav-item" id="cart">
+                <a class="nav-link" href="#"> <B></B> <img src="New folder/cart.jpg" height="30px;" width="60px;"></a>
+            </li>
+            
+        </ul>
+    </div>
+</nav>
+<!--Header Navigation Bar Finished-->
+
+    
+ 
+
+ <div class="card" style="margin-top: 20px; background-color: lightblue;" >
+  <div class="card-body" style="color: blue" >
+    <div class="row">
+        <div class= "col-xs-6 col-sm-2 col-md-2 col-lg-2">
+    <img src="New folder/pay2.png" style="width:100px;  ">
+  </div>
+  <div class= "col-xs-6 col-sm-8 col-md-8 col-lg-8">
+            <h1 class="animate__animated animate__bounce" style="margin-top: 40px; margin-left:100px; color: black;"> PAYMENT METHOD</h1>
+  </div>
+  
+    <div class= "col-xs-6 col-sm-2 col-md-2 col-lg-2">
+    <img src="New folder/cardp.png" style="width:100px;  ">
+  </div>
+</div>
+</div>
+</div>
+</div>
+
+
+
+
+<div class="card" style="margin-top: 20px; background-color: lightblue;" >
+  <div class="card-body" >
+    <div class= "col-xs-12 col-sm-12 col-md-12 col-lg-12">
+<div class= "row">
+     <div class= "col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <form>
+  <div class="form-row">
+  
+    <div class= "col-xs-12 col-sm-6 col-md-6 col-lg-6">
+      <input type="text" class="form-control" placeholder="First name" style="margin-top: 30px">
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+      <input type="text" class="form-control" placeholder="Last name" style="margin-top: 30px">
+
+
+   </div>
+    </div>
+
+
+  <div class="form-row">
+    <div class= "col-xs-12 col-sm-6 col-md-6 col-lg-6">
+      <input type="text"  class="form-control" placeholder="Phone number" style="margin-top: 20px;">
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+      <input type="text"  class="form-control" placeholder="Email" style="margin-top: 20px;">
+
+
+   </div>
+  </div>
+
+
+  <div class="form-row">
+    <div class= "col-xs-12 col-sm-4 col-md-4 col-lg-4">
+
+        <!-- Default inline 1-->
+<div class="custom-control custom-radio custom-control-inline" style="margin-top: 30px;">
+  <input type="radio" class="custom-control-input" id="defaultInline1" name="inlineDefaultRadiosExample">
+  <label class="custom-control-label" for="defaultInline1"></label>
+  <label style="margin-left: 30px;">Visa Card</label>
+  <img src="New folder/visa.png" style="width:50px; margin-left: 5px;  ">
+</div>
+</div>
+
+<!-- Default inline 2-->
+  <div class= "col-xs-12 col-sm-4 col-md-4 col-lg-4">
+<div class="custom-control custom-radio custom-control-inline" style="margin-top: 30px">
+  <input type="radio" class="custom-control-input" id="defaultInline2" name="inlineDefaultRadiosExample">
+  <label class="custom-control-label" for="defaultInline2" ></label>
+  <label style="margin-left: 30px;">Master Card</label>
+   <img src="New folder/master.png" style="width:55px; margin-left: 5px;  ">
+</div>
+</div>
+
+<!-- Default inline 3-->
+  <div class= "col-xs-12 col-sm-4 col-md-4 col-lg-4">
+<div class="custom-control custom-radio custom-control-inline" style="margin-top: 30px">
+  <input type="radio" class="custom-control-input" id="defaultInline3" name="inlineDefaultRadiosExample">
+  <label class="custom-control-label" for="defaultInline3"></label>
+  <label style="margin-left: 30px;">American Express</label>
+  <img src="New folder/america.png" style="width:55px; margin-left: 5px;  ">
+</div>
+</div>
+</div>
+
+
+<div class="form-row">
+    <div class= "col-xs-12 col-sm-6 col-md-6 col-lg-6">
+      <input type="text"  class="form-control" placeholder="Card number" style="margin-top: 20px;">
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+      <input type="text"  class="form-control" placeholder="Expire date" style="margin-top: 20px;">
+
+
+  
+  
+</div>
+</div>
+
+<div class="form-row">
+    <div class= "col-xs-12 col-sm-6 col-md-6 col-lg-6">
+      <input type="text"  class="form-control" placeholder="Name of the card" style="margin-top: 20px;">
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+      <input type="text"  class="form-control" placeholder="CCV" style="margin-top: 20px;">
+
+</div>
+</div>
+  
+</div>  
+</div>
+</div>
+
+
+
+ 
+   
+</form>
+</div>
+</div>
+
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+<div class="row">
+    
+<button type="button" class="btn btn-primary btn-lg btn-block" style="margin-top: 10px; margin-bottom: 10px; ">CONFIRM & PAY</button>
+</div>
+
+</div>
+        <!-- Footer -->
+        <footer>
+            <div class="footer-top" id="footer-top">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-3" id="sub-box">
+                            <form action="#">
+                                <div class="form-group">
+                                    <label for="email" id="sub-box">Stay in Toch....!</label>
+                                    <input type="email" class="form-control" id="email" placeholder="Enter Email"
+                                        name="email">
+                                </div>
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-primary">Subcribe Now!</button>
+                                </div>
+                            </form>
+
+                        </div>
+                        <div class="col-md-3 footer-links" id="space">
+                            <div class="row">
+                                <div class="col">
+                                    <h4>Get to Know Us</h4>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-3">
+                                    <p><a class="scroll-link" href="#" style="text-decoration: none">Company</a></p>
+                                    <p><a href="#" style="text-decoration: none">Contact</a></p>
+                                    <p><a href="#" style="text-decoration: none">FAQ</a></p>
+                                    <p><a href="#" style="text-decoration: none">Blog</a></p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-3 footer-contact" id="space">
+                            <h4>Contact</h4>
+                            <p><i class="fas fa-map-marker-alt"></i> 320/6, school lane, Walpola,Ragama-Srilanka. </p>
+                            <p><i class="fas fa-phone"></i> Phone: 94768639982 </p>
+                            <p><i class="fas fa-envelope"></i> Email: <a href="info@omg.lk"
+                                    style="text-decoration: none">info@onlineamuthukade.com</a></p>
+                            <!--<p><i class="fab fa-skype"></i> Skype: you_online</p>-->
+                        </div>
+
+                        <div class="col-md-3 col-lg-3 footer-social" id="space">
+                            <h4>Follow us</h4>
+                            <p>
+                                <a href="#" id="social"><i class="fab fa-facebook"></i></a>
+                                <a href="#" id="social"><i class="fab fa-twitter"></i></a>
+                                <a href="#" id="social"><i class="fab fa-google-plus-g"></i></a>
+                                <a href="#" id="social"><i class="fab fa-instagram"></i></a>
+                            </p>
+                            <h4>Payments</h4>
+                            <p>
+                                <a href="#"><img src="New folder/paypal.png" height="60px;" width="150px;"></a>
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!--Footer is finished-->
+
+        
+<!--Scroll Up-->
+
+<a id="back2Top" title="Back to top" href="#"><img src="Uparrow.png" height="40px;" width="40px;"></a>
+   
+</body>
+
+</html>
